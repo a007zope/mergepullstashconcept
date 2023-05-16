7c0367aa63c36f76336d1d5b9622383d19ee0dd4
P7111642 <P7111642@INAIRLT541343.ntil.com>
2023-05-16 11:40:13 +0530

    codechange

---
+++ src/main/java/feature/DemoPage.java
@@ -0,0 +1,10 @@
+package feature;
+
+public class DemoPage {
+	
+	public void order()
+	{
+		System.out.println("order id is 1123");
+	}
+
+}
---
+++ src/main/java/feature/POC.java
@@ -0,0 +1,12 @@
+package feature;
+
+public class POC {
+	
+	
+	public static void main(String[] args) {
+		
+		String name ="Aditya";
+		
+	}
+
+}
