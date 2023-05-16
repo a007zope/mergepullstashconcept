ee32096b16dee4716012ec66fcb1e1a78cfdb333
Aditya Zope <47198400+a007zope@users.noreply.github.com>
2023-05-16 12:09:15 +0530

    Create POC2.java

---
+++ src/main/java/feature/POC2.java
@@ -0,0 +1,16 @@
+  package feature;
+
+public class POCdemo {
+
+	public void demo()
+	{
+		System.out.println("Aditya");
+		int i =100;
+    int j =90;
+    System.out.println("Aditya GitHub");
+    
+    
+
+	}
+
+}
